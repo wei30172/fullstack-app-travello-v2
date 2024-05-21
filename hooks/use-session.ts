ecc5533e4d0f0eca73a03f8edf1f8d2eae6ad020
claireWei <claire610323@gmail.com>
2024-05-20 23:58:05 -0700

    update useSession hook

--- hooks/use-session.ts
+++ hooks/use-session.ts
@@ -2,14 +2,14 @@ import { useSession } from "next-auth/react"
 import { UserRole } from "@/lib/models/types"
 
 export const useCurrentUser = () => {
-  const session = useSession()
+  const { data: session } = useSession()
 
-  return session.data?.user
+  return session?.user
 }
 
 export const useCheckRole = () => {
-  const session = useSession()
+  const { data: session } = useSession()
 
-  const role = session.data?.user?.role
+  const role = session?.user?.role
   return role === UserRole.ADMIN || role === UserRole.MEMBER
 }
