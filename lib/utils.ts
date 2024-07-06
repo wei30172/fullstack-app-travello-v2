a4ad4a2c1523067d88fc6caf5ac290fec248fa40
claireWei <claire610323@gmail.com>
2024-07-05 18:46:22 -0700

    add handleSelectContentRef

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,8 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+
+export const handleSelectContentRef = (ref: HTMLDivElement | null) => {
+  if (!ref) return
+  ref.ontouchstart = (e: TouchEvent) => { e.preventDefault() }
+}
