341d78aba50238e1c005564bcd4acbd1352ca979
claireWei <claire610323@gmail.com>
2024-08-08 22:02:56 -0700

    clean

--- lib/actions/board/get-signed-url.ts
+++ lib/actions/board/get-signed-url.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3"
+import { PutObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 import s3Client from "@/lib/s3client"
