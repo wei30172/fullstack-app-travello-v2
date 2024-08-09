895218d2c04d5387398cfa793710558df4786d0b
claireWei <claire610323@gmail.com>
2024-08-09 07:37:52 -0700

    add board content skeleton

--- app/(root)/(dashboard)/board/[boardId]/page.tsx
+++ app/(root)/(dashboard)/board/[boardId]/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { BoardRole } from "@/lib/models/types"
 import { getLists } from "@/lib/actions/list/get-lists"
 
@@ -13,27 +14,45 @@ interface BoardIdPageProps {
 const BoardIdPage = async ({
   params,
 }: BoardIdPageProps) => {
-  const res = await getLists(params.boardId)
-
   return (
     <section className="p-4 h-full overflow-x-auto">
-      {"error" in res ? (
-        <div>Error loading lists: {res.error}</div>
-      ) : (
-        res.role === BoardRole.VIEWER ? (
-          <ReadOnlyListContainer
-            data={res.data}
-            role={res.role}
-          />
-        ) : (
-          <ListContainer
-            boardId={params.boardId}
-            data={res.data}
-            role={res.role}
-          />
-        )
-      )}
+       <Suspense fallback={<BoardContent.Skeleton />}>
+        <BoardContent params={params} />
+      </Suspense>
     </section>
+  )
+}
+
+const BoardContent = async ({ params }: BoardIdPageProps) => {
+  const res = await getLists(params.boardId)
+
+  if (("error" in res)) {
+    return <div>Error loading lists: {res.error}</div>;
+  }
+
+  return res.role === BoardRole.VIEWER ? (
+    <ReadOnlyListContainer data={res.data} role={res.role} />
+  ) : (
+    <ListContainer boardId={params.boardId} data={res.data} role={res.role} />
+  )
+}
+
+BoardContent.Skeleton = function SkeletonBoardContent() {
+  return (
+    <ol className="flex gap-x-3 h-full">
+      {[...Array(6)].map((_, index) => (
+        <li key={index} className="shrink-0 h-full w-[272px] select-none">
+          <div className="w-full rounded-md bg-gray-100 shadow-md py-2">
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+            <div className="h-8 bg-gray-200 rounded mt-2 mx-2" />
+          </div>
+        </li>
+      ))}
+    </ol>
   )
 }
 
